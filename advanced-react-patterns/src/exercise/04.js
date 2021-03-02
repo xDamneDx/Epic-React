989c29701841a804dcc29ffea1b2a04ddc0b73af
Владимир Ливинский <xDamneDx@Gmail.com>
2021-03-02 17:12:23 +0200

    'Advanced React Patterns' in progress...

--- advanced-react-patterns/src/exercise/04.js
+++ advanced-react-patterns/src/exercise/04.js
@@ -4,21 +4,73 @@
 import * as React from 'react'
 import {Switch} from '../switch'
 
+function callAll(...fns) {
+  return (...args) => {
+    fns.forEach(fn => {
+      fn && fn(...args)
+    })
+  }
+}
+
 function useToggle() {
   const [on, setOn] = React.useState(false)
   const toggle = () => setOn(!on)
-  const togglerProps = {'aria-pressed': on, onClick: toggle}
 
-  return {on, togglerProps}
+  const getTogglerProps = ({onClick, ...props} = {}) => {
+    return {
+      'aria-pressed': on, 
+      onClick: callAll(onClick, toggle),
+      ...props
+    }
+  }
+
+  // return {on, togglerProps: {'aria-pressed': on, onClick: toggle}}
+  return {on, getTogglerProps}
 }
 
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps} />
+//       <hr />
+//       <button aria-label="custom-button" {...togglerProps}>
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
+// function App() {
+//   const {on, togglerProps} = useToggle()
+//   return (
+//     <div>
+//       <Switch on={on} {...togglerProps({on})} />
+//       <hr />
+//       <button
+//         aria-label="custom-button"
+//         {...togglerProps}
+//         onClick={() => console.info('onButtonClick')}
+//       >
+//         {on ? 'on' : 'off'}
+//       </button>
+//     </div>
+//   )
+// }
+
 function App() {
-  const {on, togglerProps} = useToggle()
+  const {on, getTogglerProps} = useToggle()
   return (
     <div>
-      <Switch on={on} {...togglerProps} />
+      <Switch {...getTogglerProps({on})} />
       <hr />
-      <button aria-label="custom-button" {...togglerProps}>
+      <button
+        {...getTogglerProps({
+          'aria-label': 'custom-button',
+          onClick: () => console.info('onButtonClick'),
+          id: 'custom-button-id',
+        })}
+      >
         {on ? 'on' : 'off'}
       </button>
     </div>
