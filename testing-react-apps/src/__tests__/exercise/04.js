a99fafb5b9c14508b52c4832200ff2fbc193289a
Владимир Ливинский <xDamneDx@Gmail.com>
2021-03-19 13:33:08 +0200

    'Testing React Apps' in progress...

--- testing-react-apps/src/__tests__/exercise/04.js
+++ testing-react-apps/src/__tests__/exercise/04.js
@@ -4,21 +4,22 @@
 import * as React from 'react'
 import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
-import faker from 'faker'
+import {build, fake} from '@jackfranklin/test-data-bot'
 import Login from '../../components/login'
+
+const buildLoginForm = build({
+  fields: {
+    username: fake(f => f.internet.userName()),
+    password: fake(f => f.internet.password())
+  }
+})
 
 test('submitting the form calls onSubmit with username and password', () => {
   const handleSubmit = jest.fn()
 
   render(<Login onSubmit={handleSubmit} />)
 
-  const buildLoginForm = (overrides) => ({
-    username: faker.internet.userName(),
-    password: faker.internet.password(),
-    ...overrides
-  })
-
-  const { username, password } = buildLoginForm({password: '1234'})
+  const { username, password } = buildLoginForm()
 
   userEvent.type(screen.getByLabelText(/username/i), username)
   userEvent.type(screen.getByLabelText(/password/i), password)
