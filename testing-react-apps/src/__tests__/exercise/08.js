68bc7f1d61289bbbe19e172bac5fa04378fd16cc
Владимир Ливинский <xDamneDx@Gmail.com>
2021-03-24 16:13:14 +0200

    'Testing React Apps' in progress...

--- testing-react-apps/src/__tests__/exercise/08.js
+++ testing-react-apps/src/__tests__/exercise/08.js
@@ -5,20 +5,44 @@ import * as React from 'react'
 import {render, act} from '@testing-library/react'
 import useCounter from '../../components/use-counter'
 
-test('exposes the count and increment/decrement functions', () => {
-  let result
-
+function setup({initialProps} = {}) {
+  const result = {}
   function TestComponent() {
-    result = useCounter()
+    result.current = useCounter(initialProps)
     return null
   }
-
   render(<TestComponent />)
-  expect(result.count).toBe(0)
-  act(() => result.increment())
-  expect(result.count).toBe(1)
-  act(() => result.decrement())
-  expect(result.count).toBe(0)
+  return result
+}
+
+test('exposes the count and increment/decrement functions', () => {
+  const result = setup()
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(1)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
+})
+
+test('allows customization of the initial count', () => {
+  const result = setup({initialProps: {initialCount: 2}})
+
+  expect(result.current.count).toBe(2)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(2)
+})
+
+test('allows customization of the step', () => {
+  const result = setup({initialProps: {step: 3}})
+
+  expect(result.current.count).toBe(0)
+  act(() => result.current.increment())
+  expect(result.current.count).toBe(3)
+  act(() => result.current.decrement())
+  expect(result.current.count).toBe(0)
 })
 
 /* eslint no-unused-vars:0 */
