6b3b3668d53cb4e0649198fd1a25594e4fa366af
Владимир Ливинский <xDamneDx@Gmail.com>
2021-03-24 15:32:14 +0200

    'Testing React Apps' in progress...

--- testing-react-apps/src/__tests__/exercise/08.js
+++ testing-react-apps/src/__tests__/exercise/08.js
@@ -6,17 +6,48 @@ import {render, screen} from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import useCounter from '../../components/use-counter'
 
-// 🐨 create a simple function component that uses the useCounter hook
-// and then exposes some UI that our test can interact with to test the
-// capabilities of this hook
-// 💰 here's how to use the hook:
-// const {count, increment, decrement} = useCounter()
+const Counter = ({initialCount, step}) => {
+  const {count, increment, decrement} = useCounter({initialCount, step})
+
+  return (
+    <div>
+      <div>Current count: {count}</div>
+      <button onClick={decrement}>Decrement</button>
+      <button onClick={increment}>Increment</button>
+    </div>
+  )
+}
 
 test('exposes the count and increment/decrement functions', () => {
-  // 🐨 render the component
-  // 🐨 get the elements you need using screen
-  // 🐨 assert on the initial state of the hook
-  // 🐨 interact with the UI using userEvent and assert on the changes in the UI
+  render(<Counter />)
+
+  const message = screen.getByText(/Current count/i)
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const increment = screen.getByRole('button', {name: /increment/i})
+
+  expect(message).toHaveTextContent(/current count: 0/i)
+
+  userEvent.click(increment)
+  expect(message).toHaveTextContent(/current count: 1/i)
+
+  userEvent.click(decrement)
+  expect(message).toHaveTextContent(/current count: 0/i)
+})
+
+test('exposes the count and increment/decrement functions, with custom values', () => {
+  render(<Counter initialCount={2} step={3} />)
+
+  const message = screen.getByText(/Current count/i)
+  const decrement = screen.getByRole('button', {name: /decrement/i})
+  const increment = screen.getByRole('button', {name: /increment/i})
+
+  expect(message).toHaveTextContent(/current count: 2/i)
+
+  userEvent.click(increment)
+  expect(message).toHaveTextContent(/current count: 5/i)
+
+  userEvent.click(decrement)
+  expect(message).toHaveTextContent(/current count: 2/i)
 })
 
 /* eslint no-unused-vars:0 */
