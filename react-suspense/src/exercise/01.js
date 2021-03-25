68a55a3e9842ad5b07afeb2cd4f3fdbbb3a4a960
Владимир Ливинский <xDamneDx@Gmail.com>
2021-03-25 10:40:12 +0200

    'React Suspense' in progress...

--- react-suspense/src/exercise/01.js
+++ react-suspense/src/exercise/01.js
@@ -2,36 +2,19 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {fetchPokemon, PokemonDataView} from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+let pokemon
 
-// 🐨 create a variable called "pokemon" (using let)
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
-
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+const pokemonPromise = fetchPokemon('pikachu').then(pokemonData => {
+  pokemon = pokemonData
+})
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) {
+    throw pokemonPromise
+  }
 
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
@@ -46,8 +29,9 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <React.Suspense fallback={<div>Loading Pokemon...</div>}>
+          <PokemonInfo />
+        </React.Suspense>
       </div>
     </div>
   )
