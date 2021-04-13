6716196040283c0f7abcc3c69289da96d178d3a7
Владимир Ливинский <xDamneDx@Gmail.com>
2021-04-13 23:32:11 +0300

    'Build an Epic React App' in progress...

--- bookshelf/src/index.exercise.js
+++ bookshelf/src/index.exercise.js
@@ -1,9 +1,20 @@
-// 🐨 you'll need to import React and ReactDOM up here
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Logo } from './components/logo'
 
-// 🐨 you'll also need to import the Logo component from './components/logo'
+function App() {
+    return (
+        <>
+            <Logo width="80" height="80" />
+            <h1>Bookshelf</h1>
+            <div>
+                <button onClick={() => alert('Login clicked')}>Login</button>
+            </div>
+            <div>
+                <button onClick={() => alert('Register clicked')}>Register</button>
+            </div>
+        </>
+    )
+}
 
-// 🐨 create an App component here and render the logo, the title ("Bookshelf"), a login button, and a register button.
-// 🐨 for fun, you can add event handlers for both buttons to alert that the button was clicked
-
-// 🐨 use ReactDOM to render the <App /> to the root element
-// 💰 find the root element with: document.getElementById('root')
+ReactDOM.render(<App/>, document.getElementById('root'))
